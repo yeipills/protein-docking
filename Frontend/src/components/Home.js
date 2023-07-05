f28a180711aff5bb2a3c80deaf6dd4b2c30e573a
yeipills <juanpablorosasmartin@gmail.com>
2023-07-05 06:16:03 -0400

    visor stl

--- Frontend/src/components/Home.js
+++ Frontend/src/components/Home.js
@@ -1,56 +1,57 @@
 import React from "react";
 import { Card } from "primereact/card";
-//import bg from "../resources/bg.jpeg";
 import ubb_logo from "../resources/ubb_logo.png";
 
-// Define los estilos en un objeto fuera del componente de renderizado
 const styles = {
-
-
-
-    rightPane: {
-        height: "91.8%",
-        width: "38%" ,
-        marginLeft:"1%"
-    },
-    backgroundColor: {
-        backgroundColor:"#3dc484",
-    },
-    logo: {
-        marginLeft:"25%"
-    },
-    card: {
-        textAlign: "center",
-        border: "1px solid",
-        borderColor:"#8B5CF6",
-        borderRadius: "12px",
-        backgroundColor:"#cddcdc",
-        color: "black"
-    },
-    cardText: {
-        fontWeight: "light",
-        fontSize:"20px",
-        color:"#8B5CF6",
-        textTransform: "uppercase"
-    }
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh",
+    padding: "20px"
+  },
+  logoContainer: {
+    position: "absolute",
+    top: "10px",
+    right: "10px",
+  },
+  logo: {
+    width: "200px",
+    height: "150px"
+  },
+  card: {
+    textAlign: "center",
+    border: "1px solid",
+    borderColor: "#8B5CF6",
+    borderRadius: "12px",
+    backgroundColor: "#cddcdc",
+    color: "black",
+    maxWidth: "80%",
+    margin: "20px"
+  },
+  cardText: {
+    fontWeight: "light",
+    fontSize: "20px",
+    color: "#8B5CF6",
+    textTransform: "uppercase"
+  }
 };
 
 const Home = () => {
-    return (
-        <div style={styles.flexContainer}>
-            <div style={styles.leftPane}>
-                <div style={styles.background} />
-            </div>
-            <div style={styles.rightPane}>
-                <img src={ubb_logo} alt="fireSpot" width="300" height="250" style={styles.logo} />
-                <Card style={styles.card}>
-                    <span style={styles.cardText}>
-                        Análisis y comparación de los tiempos de ejecución en los algoritmos del cálculo de las formas de contexto para  mejorar la precisión de las mediciones utilizadas en el proceso de docking
-                    </span>
-                </Card>
-            </div>
-        </div>
-    );
+  return (
+    <div style={styles.container}>
+      <div style={styles.logoContainer}>
+        <img src={ubb_logo} alt="fireSpot" style={styles.logo} />
+      </div>
+      <Card style={styles.card}>
+        <span style={styles.cardText}>
+          Análisis y comparación de los tiempos de ejecución en los algoritmos del cálculo de las formas de contexto para mejorar la precisión de las mediciones utilizadas en el proceso de docking
+        </span>
+      </Card>
+      {/* Agrega aquí cualquier otro contenido adicional */}
+    </div>
+  );
 };
 
 export default Home;
