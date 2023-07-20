9226db56dcdc99b54971d3e19d050f4130e21e67
yeipills <juanpablorosasmartin@gmail.com>
2023-07-20 05:16:41 -0400

    new beta, fixed 90

--- Backend/C-lculos-Previos-main/Centroides de triangulos/Programa_python/Script03.pyx
+++ Backend/C-lculos-Previos-main/Centroides de triangulos/Programa_python/Script03.pyx
@@ -1,80 +1,41 @@
-
-
-
-
 cimport cython
 import numpy as np
 cimport numpy as np
 from libc.math cimport  sqrt, pi,sin,cos
-#import numpy as np
-import time
+
+cpdef computeCR(double radii, int delta, list centers):
+    cdef double[:] phi = np.linspace(0, 2*pi, delta)
+    cdef double[:] theta = np.linspace(0, 2*pi, delta)
+
+    cdef double[:, :] ray_directions_item = np.zeros((len(centers) * delta**2, 3))
+    cdef double[:, :] ray_origins_item = np.zeros((len(centers) * delta**2, 3))
+    cdef double[:, :] item
+    cdef int counter = 0
+    cdef double x, y, z
+
+    for item in centers:
+        for i in phi:
+            for j in theta:
+                x = radii * sin(i) * cos(j) + item[0]
+                y = radii * sin(i) * sin(j) + item[1]
+                z = radii * cos(i) + item[2]
+                ray_directions_item[counter] = [x, y, z]
+                ray_origins_item[counter] = item
+                counter += 1
+    return ray_directions_item, ray_origins_item
 
 
-
-
-cpdef computeCR(radii,delta,centers):
-        #cdef np.ndarray[DTYPE_INT_t, ndim=3] acc = np.zeros([m.size, size_max_song + size_trame + 1], dtype=DTYPE_INT)
-        #cdef int i_range = m.size
-        #cdef int j_range = active_points.shape[0]
-        #start = time.time() 
-        #cdef double[:] ray_directions_item,rayos_contexto,cr,ray_origins_item
-        #cdef double[:, :, :] ray_directions_item,rayos_contexto,cr,ray_origins_item
-        cdef list item,cr=[],rayos_contexto=[]
-        cdef int counter=0,i,j
-        cdef double x,y,z
-        phi=[]
-        theta=[]
-        ray_directions_item=[]
-        ray_origins_item=[]
-        phi=np.linspace(0, 2*pi, delta) 
-        theta=np.linspace(0, 2*pi, delta)
-
-        for item in centers:
-            for i in phi:
-                for j in theta:               
-                    x=radii*sin(i)*cos(j)+item[0]
-                    y=radii*sin(i)*sin(j)+item[1]
-                    z=radii*cos(i)+item[2]
-                    counter=counter+1
-                    ray_directions_item.append([x,y,z])
-                    ray_origins_item.append(item)
-                    final=str(x)+" "+str(y)+" "+str(z)
-                    origen=str(item[0])+" "+str(item[1])+" "+str(item[2])
-                    rayos_contexto.append(str(counter)+" "+origen+" "+final)
-            cr.append([counter,item,ray_directions_item])
-            counter=counter+1
-        return cr,rayos_contexto    
-
-
-
-       
-cpdef float compute_function_final(list cr_segments,list cr,mesh_SES):
-  
-    cdef list item
-    cdef list elem
-    cdef double[:, :] origen
-    cdef double[:, :] finales
+cpdef float compute_function_final(list cr_segments, double[:, :] ray_directions_item, double[:, :] ray_origins_item, mesh_SES):
     cdef double[:, :] vector
-    cdef int count=0 
-    cdef int n=17
+    cdef double[:, :] origen, rayo
+    cdef double[:] hits
+    cdef int n = 17
+    cdef int count = 0
 
-    for item in cr:    
-        origen=item[1]
-        finales=item[2]
-        count=count+1
-        for rayo in finales:
-            vector=np.linspace(origen,rayo,n) # Vector para dividir el rayo en segmentos
-          
-            hits=mesh_SES.ray.intersects_any(ray_origins=vector[0:len(vector)-1],
-                                    ray_directions=vector[1:len(vector)]) #212,95 segundos 4 minutos
-         
-            string1=str(item[0])
-           
-            string2=" ".join([str(elem) for elem in item[1]])
-           
-            string3=" ".join([str(elem[0])+" "+str(elem[1])+" "+str(elem[2]) for elem in vector])
-      
-            string4=" ".join([str(elem) for elem in hits])
-            
-            cr_segments.append(string1+" "+string2+" "+string3+" "+string4)                   
+    for count in range(ray_directions_item.shape[0]):
+        origen = ray_origins_item[count]
+        rayo = ray_directions_item[count]
+        vector = np.linspace(origen, rayo, n)
+        hits = mesh_SES.ray.intersects_any(ray_origins=vector[:-1], ray_directions=vector[1:])
+        cr_segments.append([count, origen, vector, hits])
     return cr_segments
--- Backend/C-lculos-Previos-main/Centroides de triangulos/Programa_python/socketServer.py
+++ Backend/C-lculos-Previos-main/Centroides de triangulos/Programa_python/socketServer.py
@@ -1,43 +1,98 @@
-from flask import Flask, request, jsonify
+import json
+from flask import Flask, request, jsonify, current_app, abort
+from flask_restful import Api, Resource
 from flask_socketio import SocketIO, emit
-from flask_cors import CORS
+from html.parser import HTMLParser
+import logging
+import bleach
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = ''
-CORS(app, resources={r"/*": {"origins": "*"}})
-
+api = Api(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
-thread = None
 
 
-@app.get("/")
-def read_root():
-    return {"Backend": "Proyecto titulo"}
+class HelloWorldResource(Resource):
+    def get(self):
+        try:
+            return jsonify({'hello': 'world'}), 200
+        except ValueError as e:
+            current_app.logger.error(str(e))
+            abort(400, str(e))
+        except KeyError as e:
+            current_app.logger.error(str(e))
+            abort(404, str(e))
+        except Exception as e:
+            current_app.logger.error(str(e))
+            abort(500, str(e))
 
 
-@app.get("/terminaParteUno")
-def termina_parte_uno():
-    socketio.emit("termina_procesar_uno")
-    return
+@app.errorhandler(400)
+def handle_bad_request(error):
+    response = jsonify({'error': 'Bad Request'})
+    response.status_code = 400
+    return response
 
 
-@app.get("/terminaParteDos")
-def termina_parte_dos():
-    socketio.emit("termina_procesar_dos")
-    return
+@app.errorhandler(404)
+def handle_not_found(error):
+    response = jsonify({'error': 'Not Found'})
+    response.status_code = 404
+    return response
 
 
-@socketio.on("connect")
-def connected():
-    """event listener when client connects to the server"""
-    print(request.sid)
-    print("client has connected")
-    emit("connected_user", request.sid)
+@app.errorhandler(500)
+def handle_internal_server_error(error):
+    response = jsonify({'error': 'Internal Server Error'})
+    response.status_code = 500
+    return response
 
 
-@socketio.on("disconnect")
-def disconnected():
-    print("user disconnected")
-    emit("disconnect")
+api.add_resource(HelloWorldResource, '/')
+
+logger = logging.getLogger(__name__)
+
+
+def validate_data(data):
+    if data is None:
+        return False
+    if not isinstance(data, str):
+        raise TypeError(f'Invalid data type: {type(data)}')
+
+    cleaned_data = bleach.clean(data)
+
+    return True
+
+
+def handle_message(data):
+    logger.info('received message: %s', data)
+
+    if not validate_data(data):
+        return
+
+    try:
+        pass
+    except Exception as e:
+        logger.error('An error occurred: %s', str(e))
+        socketio.emit('response', f'Error: {str(e)}')
+        return
+
+    response = f'Message received: {data}'
+    socketio.emit('response', response)
+
+
+@socketio.on('connect')
+def handle_connect():
+    logger.debug('Client connected')
+    try:
+        emit('connection_success', {'message': 'Connected successfully'})
+    except Exception as e:
+        logger.error(f'Error during connection: {e}')
+        emit('connection_error', {'message': str(e)})
+
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    logger.debug('Client disconnected')
 
 
 if __name__ == '__main__':
